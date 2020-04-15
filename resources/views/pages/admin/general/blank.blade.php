017baa843fa47793ca5ed438405c42e61edf60b1
manoch viriya <bosssa5558@gmail.com>
2020-04-15 22:51:52 +0700

    test select search

--- resources/views/pages/admin/general/blank.blade.php
+++ resources/views/pages/admin/general/blank.blade.php
@@ -1,56 +1,58 @@
 @extends('layouts.admin.master')
 
 @push('title')
-  Krukidee | หน้าหลัก
+Krukidee | หน้าหลัก
 @endpush
 
 @push('plugin-styles')
-  <!-- Plugin css import here -->
-  {!! Html::style('admin/assets/plugins/select2/select2.min.css') !!}
+{!! Html::script('admin/js/app.js') !!}
+<!-- Plugin css import here -->
+{!! Html::style('admin/assets/plugins/select2/select2.min.css') !!}
 @endpush
 
 @section('content')
-  <!-- Page content here -->
-  <div class="row">
+<!-- Page content here -->
+<div class="row">
     <div class="col-md-12 grid-margin stretch-card">
-      <div class="card">
-        <div class="card-body">
-          <h4 class="card-title">Select 2</h4>
-          <p class="card-description">Read the <a href="https://select2.org/" target="_blank"> Official Select2 Documentation </a>for a full list of instructions and other options.</p>
-          <div class="form-group">
-            <label>Single select box using select 2</label>
-            <select class="js-example-basic-single w-100">
-              <option value="TX">Texas</option>
-              <option value="NY">New York</option>
-              <option value="FL">Florida</option>
-              <option value="KN">Kansas</option>
-              <option value="HW">Hawaii</option>
-            </select>
-          </div>
-          <div class="form-group">
-            <label>Multiple select using select 2</label>
-            <select class="js-example-basic-multiple w-100" multiple="multiple">
-              <option value="TX">Texas</option>
-              <option value="WY">Wyoming</option>
-              <option value="NY">New York</option>
-              <option value="FL">Florida</option>
-              <option value="KN">Kansas</option>
-              <option value="HW">Hawaii</option>
-            </select>
-          </div>
+        <div class="card">
+            <div class="card-body">
+                <h4 class="card-title">Select 2</h4>
+                <p class="card-description">Read the <a href="https://select2.org/" target="_blank"> Official Select2
+                        Documentation </a>for a full list of instructions and other options.</p>
+                <div class="form-group">
+                    <label>Single select box using select 2</label>
+                    <select class="js-example-basic-single w-100">
+                        <option value="TX">Texas</option>
+                        <option value="NY">New York</option>
+                        <option value="FL">Florida</option>
+                        <option value="KN">Kansas</option>
+                        <option value="HW">Hawaii</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label>Multiple select using select 2</label>
+                    <select class="js-example-basic-multiple w-100" multiple="multiple">
+                        <option value="TX">Texas</option>
+                        <option value="WY">Wyoming</option>
+                        <option value="NY">New York</option>
+                        <option value="FL">Florida</option>
+                        <option value="KN">Kansas</option>
+                        <option value="HW">Hawaii</option>
+                    </select>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
+</div>
 
 @endsection
 
 @push('plugin-scripts')
-  <!-- Plugin js import here -->
-  {!! Html::script('admin/assets/plugins/select2/select2.min.js') !!}
+<!-- Plugin js import here -->
+{!! Html::script('admin/assets/plugins/select2/select2.min.js') !!}
 @endpush
 
 @push('custom-scripts')
-  <!-- Custom js here -->
-  {!! Html::script('admin/assets/js/select2.js') !!}
+<!-- Custom js here -->
+{!! Html::script('admin/assets/js/select2.js') !!}
 @endpush
